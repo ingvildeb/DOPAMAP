097b2a650a25de9b7728141e2cef5ac467d3e286
Ingvild Bjerke <i.e.bjerke@medisin.uio.no>
2023-08-03 18:40:19 +0200

    Clean up of script
    Completed clean-up / documentation of script, removed parts that are generic

--- DOPAMAP_graphing.py
+++ DOPAMAP_graphing.py
@@ -44,15 +44,6 @@ read_customregs = pd.read_csv(customregsfile, sep = ";")
 regions_to_hierarchy_dict = dict(zip(read_customregs['Region name'], read_customregs['Hierarchy']))
 
 
-## Get counts and mean values per age group
-D1R_count = (D1R_densities.groupby("age").count()).iloc[0:,2:]
-D2R_count = (D2R_densities.groupby("age").count()).iloc[0:,2:]
-
-D1R_densities_mean = D1R_densities.groupby("age").mean()
-D2R_densities_mean = D2R_densities.groupby("age").mean()    
-
-D1R_densities_hier_mean = D1R_densities_hier.groupby("age").mean()
-D2R_densities_hier_mean = D2R_densities_hier.groupby("age").mean()
 
 
 
@@ -189,6 +180,21 @@ plt.show()
 
 
 
+
+
+##################### ANALYSIS OF FINER SUBREGIONS (for bar graphs, supplementary data)
+
+## Get counts and mean values per age group
+D1R_count = (D1R_densities.groupby("age").count()).iloc[0:,2:]
+D2R_count = (D2R_densities.groupby("age").count()).iloc[0:,2:]
+
+D1R_densities_mean = D1R_densities.groupby("age").mean()
+D2R_densities_mean = D2R_densities.groupby("age").mean()    
+
+D1R_densities_hier_mean = D1R_densities_hier.groupby("age").mean()
+D2R_densities_hier_mean = D2R_densities_hier.groupby("age").mean()
+
+
 # LINE PLOTS WITH FINE REGIONS ORGANIZED PER MEDIUM LEVEL HIERARCHICAL REGION
 maxval_dict_medium_hier = grf.create_maxval_dict(customregsfile, "Hierarchy", "Region name", [D1R_densities_mean, D2R_densities_mean])
 
@@ -206,7 +212,7 @@ grf.create_line_graphs_per_hierarchy_level(customregsfile, "Hierarchy_major", "H
 
 ##### BAR PLOTS PER AGE (used in Figure 2 and Supplementary material)
 
-color_list = grf.set_region_colors(customregsfile)
+color_list = grf.set_region_colors(customregsfile, file_type="csv")
 region_list = grf.set_region_names(customregsfile)    
 
 # create plots for D1R densities
@@ -260,68 +266,6 @@ grf.create_allen_hbar_graph("D2R_P70", input_dir = D2R_P70_densities_mean, color
 
 
 
-#### CREATE LINE PLOTS FOR HIGHER HIERARCHICAL REGIONS
-
-
-import pandas as pd
-import mpld3
-from mpld3 import plugins
-from mpld3.utils import get_id
-import numpy as np
-import collections
-import matplotlib.pyplot as plt
-from matplotlib.pyplot import figure  
-from mpl_toolkits.mplot3d import Axes3D
-import nutil_scripts.graphing_functions as grf
-
-def unique_list(sequence):
-    seen = set()
-    return [x for x in sequence if not (x in seen or seen.add(x))]
-
-
-### D1R
-color_list_hier = grf.set_region_colors(customregsfile, file_type = "csv", R_col = "R_hier", G_col = "G_hier", B_col = "B_hier")
-color_list_hier = unique_list(color_list_hier)
-ls_list_hier = ['-', ':','--', '-.', '-', ':','--', '-.', '-', ':','--', '-.', '-', ':','--', '-.', '-', ':','--', '-.', '-', ':','--', '-.', '-', ':']
-
-#matplotlib.rcParams.update({'font.size': 22})
-
-
-
-fig = plt.figure(figsize = (25,25), dpi=80)
-
-ax1 = fig.add_subplot(2,1,1)
-
-l1 = []
-columns = []
-for column, color, lstyle in zip(D1R_densities_hier_mean, color_list_hier, ls_list_hier):
-    data = D1R_densities_hier_mean[column] 
-    l1.append(ax1.plot(data, c = color, linewidth = 2.5, ls = lstyle))
-    columns.append(column)
-    
-ax1.set_ylim(0, 25000)
-#ax1.rc('font', size=28)
-#plt.legend(loc=(1.04, 0))
-plugins.connect(fig, plugins.InteractiveLegendPlugin(l1, columns, ax=ax1,  start_visible=False))
-mpld3.save_html(fig, 'bigtest.html')
-# plt.savefig('D1R_devplot_wholebrain.svg', bbox_inches='tight')    
-# plt.show()    
-    
-    
-### D2R
-
-
-figure(figsize = (30,30), dpi=80)
-
-for column, color, lstyle in zip(D2R_densities_hier_mean, color_list_hier, ls_list_hier):
-    data = D2R_densities_hier_mean[column] 
-    plt.plot(data, c = color, label = column, linewidth = 2.5, ls = lstyle)
-    
-plt.ylim(0, 25000)
-plt.rc('font', size=40)
-plt.legend(loc=(1.04, 0))
-plt.savefig('D2R_devplot_wholebrain.svg', bbox_inches='tight')    
-plt.show()    
 
 
 
