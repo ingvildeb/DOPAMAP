d4f553543d9854a9a717bfcea59b9ce749d8c226
Ingvild Bjerke <i.e.bjerke@medisin.uio.no>
2024-09-03 14:33:02 +0200

    Update create_ratio_volumes.py

--- scripts/create_ratio_volumes.py
+++ scripts/create_ratio_volumes.py
@@ -9,7 +9,8 @@ import nibabel as nib
 import numpy as np
 import matplotlib.pyplot as plt 
 import matplotlib as mpl
-from scipy.ndimage import zoom
+from skimage import feature
+from tqdm import tqdm
 
 d1_path = r"C:\Users\ingvieb\dopamap\D1_volumes_averaged.nii"
 d1_vol = nib.load(d1_path)
@@ -23,7 +24,7 @@ d2_vol_data = d2_vol.get_fdata()
 d2_vol_data[d2_vol_data<0] = 0
 
 
-pc99 = np.percentile((d1_vol_data, d2_vol_data), 99.9)
+pc99 = np.percentile((d1_vol_data, d2_vol_data), 99)
 
 d1_vol_data[d1_vol_data>pc99] = pc99
 
@@ -39,9 +40,11 @@ d2_normalized = ((d2_vol_data - np.nanmin(d2_vol_data)) / d2_range)
 
 max_val = np.nanmax((d1_normalized,d2_normalized))
 
-ratio_vol = 1 - (((d1_normalized - d2_normalized)/max_val))
+#ratio_vol = 1 - (((d1_normalized - d2_normalized)/max_val))
 
 
+ratio_vol = (d1_vol_data - d2_vol_data) / (d1_vol_data + d2_vol_data)
+#ratio_vol = (d1_normalized - d2_normalized) / (d1_normalized + d2_normalized)
 
 ratio_vol[~(ratio_vol % 7).astype(bool)] = np.nan
 
@@ -54,29 +57,8 @@ plt.colorbar()
 plt.show()
 
 
-vol_for_header = nib.load(r"C:\Users\ingvieb\dopamap\annotation_boundary_10.nii.gz")
-
-micron_size = 0.001 * 25 # i changed this to 10 from 25 for the zoom method below. 
-header = vol_for_header.header 
-header.set_xyzt_units(xyz=2, t=0)
-header['pixdim'][1:4] = np.array([micron_size, micron_size, micron_size])
-header['dim'] = d1_vol.header['dim']
-header['srow_x'] = np.array([0.025,0,0, -5.695])
-header['srow_y'] = np.array([0,0.025,0, 5.35])
-header['srow_z'] = np.array([0,0,-0.025, -5.22])
-vol_for_header.affine[:,:3] *= 2.5
-vol_for_header.affine[:3,:3] = np.array([[0.025,0,0],
-                                                       [0.0,0.025,0],
-                                                       [0.0,0,0.025]])
-print(header)
-out_img = nib.Nifti1Image(ratio_vol, vol_for_header.affine, header)
+out_img = nib.Nifti1Image(ratio_vol, d1_vol.affine)
 nib.save(out_img, r"C:\Users\ingvieb\dopamap\ratio_vol.nii.gz")
-
-
-
-print(d1_vol.header)
-
-
 
 
 
